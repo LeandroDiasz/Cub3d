27d92d038a42eeb8ae20db3de3a4236a99131bcd
Leandrodiasz <leandrodiasrp1@gmail.com>
2025-06-10 17:04:35 -0300

    feat: implement main function for game initialization and execution

---
+++ main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leandrodias <leandrodias@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/10 16:41:59 by leandrodias       #+#    #+#             */
+/*   Updated: 2025/06/10 16:55:22 by leandrodias      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	main(int argc, char **argv)
+{
+    t_cub3d	game;
+
+    if (argc != 2)
+    {
+        ft_putstr_fd("Error: Invalid number of arguments\n", STDERR_FILENO);
+        return (EXIT_FAILURE);
+    }
+    if (init_cub3d(&game, argv[1]) == EXIT_FAILURE)
+        return (EXIT_FAILURE);
+    start_game(&game);
+    return (EXIT_SUCCESS);
+}
