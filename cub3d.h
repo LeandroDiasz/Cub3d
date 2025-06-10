4a0cf7dff23006a9f941377ecade55e3ee77095e
Leandrodiasz <leandrodiasrp1@gmail.com>
2025-06-10 16:38:19 -0300

    feat: add cub3d header file with key definitions and includes

---
+++ cub3d.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leandrodias <leandrodias@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/10 15:57:26 by leandrodias       #+#    #+#             */
+/*   Updated: 2025/06/10 16:23:06 by leandrodias      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "minilibx-linux/mlx.h"
+# include "Libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
+
+
+#define ESC_KEY 65307
+#define KEY_ARROW_RIGHT 100
+#define KEY_ARROW_LEFT 97
+#define KEY_ARROW_UP 119
+#define KEY_ARROW_DOWN 115
+
+#endif
