f63fc0e87bf04f028944e690f99c63749adfef4e
ledias-d <leocraftjg@gmai.com>
2025-09-13 18:27:12 -0300

    fix(init_prop.c): update author information and enhance texture validation logic

--- src/parser/init_prop.c
+++ src/parser/init_prop.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_prop.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: ledias-d <ledias-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 19:40:08 by ledias-d          #+#    #+#             */
-/*   Updated: 2025/09/01 17:41:29 by codespace        ###   ########.fr       */
+/*   Updated: 2025/09/06 11:21:08 by ledias-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ static int set_textures(char **file, t_cub3d *game, int *y)
 {
 	while (file[*y])
 	{
+		if (file[*y][0] == '\n' || file[*y][0] == '\0')
+		{
+			(*y)++;
+			continue;
+		}
 		if (ft_strncmp(file[*y], "NO ", 3) == 0)
 			game->no_texture = ft_strtrim(file[*y] + 3, "\n");
 		else if (ft_strncmp(file[*y], "SO ", 3) == 0)
@@ -28,8 +33,6 @@ static int set_textures(char **file, t_cub3d *game, int *y)
 			game->floor_color = ft_strtrim(file[*y] + 2, "\n");
 		else if (ft_strncmp(file[*y], "C ", 2) == 0)
 			game->ceiling_color = ft_strtrim(file[*y] + 2, "\n");
-		else if (file[*y][0] == '\n')
-			(*y)++;
 		else
 			break ;
 		(*y)++;
@@ -37,14 +40,58 @@ static int set_textures(char **file, t_cub3d *game, int *y)
 	return (1);
 }
 
+static int	validate_required_textures(t_cub3d *game)
+{
+	if (!game->no_texture)
+	{
+		printf("Error: North texture (NO) not defined\n");
+		return (0);
+	}
+	if (!game->so_texture)
+	{
+		printf("Error: South texture (SO) not defined\n");
+		return (0);
+	}
+	if (!game->we_texture)
+	{
+		printf("Error: West texture (WE) not defined\n");
+		return (0);
+	}
+	if (!game->ea_texture)
+	{
+		printf("Error: East texture (EA) not defined\n");
+		return (0);
+	}
+	if (!game->floor_color)
+	{
+		printf("Error: Floor color (F) not defined\n");
+		return (0);
+	}
+	if (!game->ceiling_color)
+	{
+		printf("Error: Ceiling color (C) not defined\n");
+		return (0);
+	}
+	return (1);
+}
+
 int	cub_validate(char **file, t_cub3d *game)
 {
 	int	y;
+	
 	y = 0;
 	if (!set_textures(file, game, &y))
 		return (0);
-	while (file[y] && file[y][0] == '\n')
-		(y)++;
+	if (!validate_required_textures(game))
+		return (0);
+	while (file[y] && (file[y][0] == '\n' || file[y][0] == '\0' || 
+		   (ft_strlen(file[y]) == 1 && file[y][0] == ' ')))
+		y++;
+	if (!file[y])
+	{
+		printf("Error: No map found in file\n");
+		return (0);
+	}
 	game->map = &file[y];
 	if (!game->map || !*game->map)
 		return (0);
